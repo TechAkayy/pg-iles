4404158ac8dd2ddbe1b5fe05eb4f340754f0f845
Akayy <techakayy@gmail.com>
2023-03-01 10:54:19 +1100

    1.5 Dark Mode with VueUse

---
+++ src/components/ToggleDarkMode.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+	import { computed } from 'vue'
+	import { useDark, useToggle } from '@vueuse/core'
+
+	const isDark = useDark()
+	const toggleDark = useToggle(isDark)
+
+	const modeIcon = computed(() =>
+		isDark.value ? 'ic:twotone-dark-mode' : 'ic:twotone-light-mode'
+	)
+</script>
+<template>
+    <BaseButton @click="toggleDark()">
+        <span>Toggle Mode</span>
+        <template #suffix-icon>
+            <BaseIcon width="20px" class="dark-mode-icon" :name="modeIcon" style="margin-left: 8px"/>
+        </template>
+    </BaseButton>
+</template>
+<style scoped>
+	.dark-mode-icon {
+		display: inline-block;
+		margin-left: 13px;
+		align-items: center;
+	}
+</style>
