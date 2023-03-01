ecad50df28092e4a0412cdb84a3066d502711bfb
Akayy <techakayy@gmail.com>
2023-03-01 11:38:21 +1100

    1.9 Add Prettier for formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ['@nuxt/eslint-config'],
+  extends: ['@nuxt/eslint-config', 'prettier'],
   rules: {
 		// override/add rules settings here, such as:
 		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
